cf364897c180cebf532457260f9b1e113f40cf05
rajariandhana <rajariandhana@gmail.com>
2024-07-19 18:35:13 +0700

    com

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,25 +3,72 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Gloudemans\Shoppingcart\Facades\Cart;
+// use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CartController extends Controller
 {
     public function store(Request $request){
         // dont forget to validate
-        $product = Product::findOrFail($request->input('product_id'));
-        Cart::add(
-            $product->product_id,
-            $product->name,
-            $request->input('quantity'),
-            $product->price,
-            0,
-            [
-                'size'=>$request->input('Size'),
-                'color'=>$request->input('Color')
-            ]
-        );
-        return redirect('home');
+        // $product = Product::findOrFail($request->input('product_id'));
+        // Cart::add(
+        //     $product->product_id,
+        //     $product->name,
+        //     $request->input('quantity'),
+        //     $product->price,
+        //     0,
+        //     [
+        //         'size'=>$request->input('Size'),
+        //         'color'=>$request->input('Color')
+        //     ]
+        // );
+        // return redirect('home');
+        $product=Product::findOrFail($request->input('product_id'));
+        $product_id=$request->input('product_id');
+        $selectedSize=$request->input('selectedSize');
+        $selectedColor=$request->input('selectedColor');
+
+        // dump($product_id);
+        // dump($selectedSize);
+        // dump($selectedColor);
+        $key=self::CreateKey($product_id,$selectedSize,$selectedColor);
+        // dump($key);
+
+        // $parsed_product_id;
+        // $parsed_selectedSize;
+        // $parsed_selectedColor;
+        // self::ParseKey($key, $parsed_product_id,$parsed_selectedSize,$parsed_selectedColor);
+        // dump($parsed_product_id);
+        // dump($parsed_selectedSize);
+        // dump($parsed_selectedColor);
+    }
+    private function AddItem($key){
+        $value = session($key,0);
+        session([$key=>$value++]);
+    }
+    private function RemoveItem($key){
+        $value = session($key,1);
+        session([$key=>$value--]);
+    }
+    private function CreateKey($product_id, $selectedSize, $selectedColor){
+        // $key = "|cart|".$product_id.'|'.$selectedColor.'|'.$selectedSize.'|';
+        $key = "|cart|$product_id|s:$selectedSize|c:$selectedColor|";
+        return $key;
+    }
+    
+    private function ParseKey(string $s, &$product_id, &$selectedSize, &$selectedColor){
+        // key: |cart|product_id|s:|c:|
+        // value: quantity
+        $splits = explode('|',$s);
+        $product_id=$splits[2];
+        $selectedSize=$splits[3];
+        $selectedColor=$splits[4];
+
+        $selectedSize = substr($selectedSize,2);
+        $selectedColor = substr($selectedColor,2);
+        // dump($product_id);
+        // dump($selectedSize);
+        // dump($selectedColor);
     }
 }
---
+++ resources/views/components/cart-item.blade.php
@@ -0,0 +1,7 @@
+<div class="flex flex-row items-center px-4 py-2 bg-white rounded-md">
+    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="" class="object-cover object-center w-4 h-4 lg:h-6 lg:w-6">
+    <h4>{{$product->name}}</h4>
+    <div>qty</div>
+    <div>{{$product->price}}</div>
+    <div>{{$product->price}} * qty</div>
+</div>
