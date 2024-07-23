2ab997af299d8b1db025d4ae95f913b9d73a7dda
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2024-07-23 12:48:53 +0700

    done?

--- resources/views/livewire/cart-item.blade.php
+++ resources/views/livewire/cart-item.blade.php
@@ -1,16 +1,23 @@
-<div x-data="{ quantity: @entangle('quantity'), price: @entangle('price'),
-    checkQuantity(){
+<div x-data="{
+    quantity: @entangle('quantity'),
+    price: @entangle('price'),
+    checkQuantity() {
         if (this.quantity < 1) {
             this.quantity = 1;
-        }
-        else if (this.quantity > 10) {
+        } else if (this.quantity > 10) {
             this.quantity = 10;
         }
-        }
-    }"
-    class="flex flex-row items-center justify-between px-4 py-2 rounded-md bg-red-50">
-    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt=""
-        class="object-cover object-center w-4 h-4 lg:h-6 lg:w-6">
+    }
+}" class="flex flex-row items-center justify-between px-4 py-2 rounded-md bg-red-50">
+    @php
+        $src = url('/') . '/products/' . $product->product_id . '.avif';
+    @endphp
+    @if (!$src)
+        @php
+            $src = 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg';
+        @endphp
+    @endif
+    <img src="{{ $src }}" alt="" class="object-cover object-center w-4 h-4 lg:h-6 lg:w-6">
     <div class="flex flex-col gap-0 bg-blue-50">
         <h4 class="m-0 bg-yellow-50">{{ $product->name }}</h4>
         <p class="text-sm text-gray-400 bg-orange-50">{{ $item['variant'] }}</p>
@@ -27,7 +34,7 @@
         <input type="number" x-model="quantity" min="1"max="10"
             class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm block w-12 py-2.5"
             value="{{ $item['quantity'] }}" required />
-            <button @click="quantity < 10 ? quantity++ : quantity = 10; checkQuantity(); $wire.setQuantity()"
+        <button @click="quantity < 10 ? quantity++ : quantity = 10; checkQuantity(); $wire.setQuantity()"
             class="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11">
             <svg class="w-3 h-3 text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 18 18">
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,13 +1,21 @@
 <x-layout>
     <div class="max-w-screen-xl py-4 mx-auto lg:px-6">
-
         <div class="flex flex-col gap-4 md:flex-row">
             <div
                 class="overflow-hidden bg-gray-200 rounded-tl-xl rounded-tr-xl lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-                    alt="" class="object-cover object-center w-full h-full lg:h-full lg:w-full">
+                @php
+                    $src = url('/') . '/products/' . $product->product_id . '.avif';
+                @endphp
+                @if (!$src)
+                @php
+                    $src = "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg";
+                @endphp
+                @endif
+                <img src="{{ $src }}" alt=""
+                        class="object-cover object-center w-full h-full lg:h-full lg:w-full">
+
             </div>
-            <livewire:product-form :product="$product"/>
+            <livewire:product-form :product="$product" />
             {{-- {{$product->name}} --}}
             {{-- @livewire(ProductForm::class,['product'=>$product]) --}}
         </div>
